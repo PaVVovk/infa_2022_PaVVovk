2bc4b3ff99acbfc4e06e028051e06baaa784e52e
PaVVovk <vovk.pa@phystech.edu>
2022-11-12 23:12:40 +0300

    Added the changes, described in previous commit (that commit failed)

--- lab8/catch_the_ball.py
+++ lab8/catch_the_ball.py
@@ -5,10 +5,13 @@ from random import randint
 pygame.init()
 
 pygame.font.init()
-font = pygame.font.SysFont('Comic Sans MS', 30) #инициализация шрифта для надписи очков
+font = pygame.font.SysFont('Comic Sans MS', 30)         #инициализация шрифта для надписи очков
 
-FPS = 2     #число обновлений кадров в секунду
-screen = pygame.display.set_mode((1200, 800))
+FPS = 20                                                #число обновлений кадров в секунду
+
+WIDTH = 1200                                            #ширина окна
+HEIGHT = 800                                            #высота окна
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -17,9 +20,9 @@ GREEN = (0, 255, 0)
 MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 WHITE = (255, 255, 255)
-COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN] #набор цветов для шаров
+COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]      #набор цветов для шаров
 
-score = 0 # начальное значение очков игрока
+score = 0                                               # начальное значение очков игрока
 
 def dist(dot1, dot2):
     '''
@@ -42,32 +45,57 @@ def show_score():
 def click(event):
     '''
     Обрабатывает нажатие левой кнопки мыши.
-    В данной версии - увеличивает очки игрока при нажатии в пределах шара.
+    В данной версии - увеличивает очки игрока при нажатии в пределах шара,
+    а затем генерирует новый шар.
     '''
     global score
-    if dist(event.pos, (x, y)) < r: score += 1
+    if dist(event.pos, (x, y)) < r:
+        score += 1
+        new_ball()
         
 
 def new_ball():
     '''
-    Рисует новый шарик радиуса r, цвета color
-    и с центром в точке (x, y).
-    Цвет, радиус и координаты центра выбираются случайно.
+    Рисует новый шарик радиуса r, цвета color,
+    с центром в точке (x, y) и скоростью по осям dx и dy.
+    Цвет, радиус, скорость и координаты центра выбираются случайно.
     '''
-    global x, y, r
-    x = randint(100, 1100)
-    y = randint(100, 700)
-    r = randint(10, 100)
-    color = COLORS[randint(0, 5)]
-    circle(screen, color, (x, y), r)
+    global x, y, dx, dy, r, color
 
-screen.fill(WHITE) #заполняет стартовый экран белым цветом
+    r = randint(10, 100)
+    
+    x = randint(r, WIDTH-r)
+    y = randint(r, HEIGHT-r)
+
+    dx = randint(10,30)
+    dy = randint(10,30)
+
+    color = COLORS[randint(0, 5)]
+    
+
+def speed():
+    '''
+    Осуществляет изменение положения шарика
+    и отскакивание от стенок.
+    '''
+    global x, y, dx, dy, r
+    if (x > WIDTH - r) or (x < r):
+        dx *= -1
+    if (y > HEIGHT - r) or (y < r):
+        dy *= -1
+    x += dx
+    y += dy
+
+
+screen.fill(WHITE)                      #заполняет стартовый экран белым цветом
+new_ball()                              #создание первого шара
 pygame.display.update()
 
 clock = pygame.time.Clock()
 finished = False
 
 while not finished:
+    circle(screen, color, (x, y), r)
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -75,9 +103,9 @@ while not finished:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             click(event)
     show_score()
-    new_ball()
+    speed()
     
     pygame.display.update()
-    screen.fill(WHITE) #освобождает экран для следующего шага
+    screen.fill(WHITE)                  #освобождает экран для следующего шага
 
 pygame.quit()
